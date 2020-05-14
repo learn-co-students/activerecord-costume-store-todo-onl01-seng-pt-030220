94bd491da42a6f100b7ce73f1d7db06cd7d6d201
cdominick091984 <cdominick091984@gmail.com>
2020-05-14 11:24:18 -0400

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
             t.datetime :opening_date
             t.datetime :closing_date
             t.string :description
+            t.timestamps
         end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -40,6 +42,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
