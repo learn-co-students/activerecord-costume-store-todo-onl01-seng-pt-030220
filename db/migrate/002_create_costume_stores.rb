e898bbac40dd331813ae8fd6a9a1bacadda0841b
Sara Garcia <sarisgar28@gmail.com>
2020-05-27 16:10:05 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,20 @@
 # Create your costume_stores migration here
+
+class CreateCostumesStores < ActiveRecord::Migration[5.2]
+
+    def change 
+        create_table :costume_stores do |t|
+         t.string :name 
+         t.string :location 
+         t.string :costume_inventory
+         t.integer :number_of_employees
+         t.string :whether_or_not_its_still_in_business
+         t.integer :opening_time
+         t.integer :closing_time
+
+        end 
+    end
+    
+
+
+end 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+ 
+class HauntedHouses < ActiveRecord::Migration[5.2]
+
+    def change 
+     create_table :haunted_houses do |t|
+        t.string :name 
+        t.string :location
+        t.string :theme 
+        t.integer :price 
+        t.string :whether_theyre_family_friendly_or_not
+        t.integer :opening_date 
+        t.integer :closing_date
+        t.string :long_description 
+    
+       end
+     end 
+end 
